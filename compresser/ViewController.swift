b8aa5ef85a9ab7dd133ee7b28970a77b43983a7b
mishacausur <mishacausur@mail.ru>
2026-01-26 19:15:44 +0500

    Initial Commit

---
+++ compresser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  compresser
+//
+//  Created by Misha Causur on 26.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
